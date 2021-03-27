44e52235e159b5f95ac9436a4cdb5cb64b8e59c3
Gergely Z. Toth <gwergilius@gmail.com>
2021-03-27 12:30:23 +0100

    Hello World Blazor app

---
+++ src/Calculator/Calculator/Pages/CodeBehind/Counter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Calculator.Pages.CodeBehind
+{
+    public class Counter : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
